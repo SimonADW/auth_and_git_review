4087a282da792e40ab65cdbcbb2e8dfb19ec341e
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-05-08 10:52:21 +0200

    Set up firebase and connect

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
-console.log("Welcome");
+import { firebaseConfig } from "./firebaseConfig"
+
+import { initializeApp } from "firebase/app"
+
+import { getAuth } from "firebase/auth"
+
+initializeApp(firebaseConfig)
+
+const authService = getAuth();
---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCOipOi51m2xfq5OFz4ge_JpB-J-YrLNXk",
+	authDomain: "auth-and-git-review.firebaseapp.com",
+	projectId: "auth-and-git-review",
+	storageBucket: "auth-and-git-review.appspot.com",
+	messagingSenderId: "286975694383",
+	appId: "1:286975694383:web:688d35c2d66533a9c6d8da"
+  };
